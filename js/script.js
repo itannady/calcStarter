88079d5068a46f7eff2c645a0e5ca7a8807c61ab
Isabella Tannady <isabellatannady@gmail.com>
2022-04-27 16:52:05 -0700

    feat(operator): add operator functions

--- js/script.js
+++ js/script.js
@@ -13,21 +13,105 @@
 			3. Clear 'display' variable
 		    */
 
-//Function that takes three parameters
-function calculator(num1, operator, num2) {
-  if (operator === "+") {
-    return num1 + num2;
-  } else if (operator === "-") {
-    return num1 - num2;
-  } else if (operator === "*") {
-    return num1 * num2;
-  } else if (operator === "/") {
-    return num1 / num2;
-  } else if (operator === "&radic") {
-    return Math.pow(num1, 1 / 2);
-  } else {
-    return "error";
-  }
+//Select elements
+const output = document.querySelector("#text-output");
+// let buttons = document.querySelectorAll(".btn");
+// let operator = document.querySelectorAll('.btn1');
+let clear = document.getElementById("clear");
+
+function display(x) {
+  output.innerHTML += x;
 }
 
-console.log(calculator(1, "+", 2));
+function calculate() {
+  let screen = [];
+
+
+  if(output.innerHTML.includes("+")) {
+    screen = output.innerHTML.split("+");
+    return Number(screen[0]) + Number(screen[1]);
+    
+  }
+  if(output.innerHTML.includes("-")) {
+    screen = output.innerHTML.split("-");
+    return Number(screen[0]) - Number(screen[1]);
+    
+  }
+  if(output.innerHTML.includes("*")) {
+    screen = output.innerHTML.split("*");
+    return Number(screen[0]) * Number(screen[1]);
+ 
+  }
+  if(output.innerHTML.includes("/")) {
+    screen = output.innerHTML.split("/");
+    return Number(screen[0]) * Number(screen[1]);
+ 
+  }
+  if(output.innerHTML.includes("√")) {
+    // screen = output.innerHTML.split("√");
+    return Math.pow(Number(screen[0]), 1 / 2);
+ 
+  }
+
+}
+
+console.log(screen)
+
+function equal() {
+let x = calculate();
+output.innerHTML = x;
+
+}
+
+//Clear function
+clear.addEventListener("click", () => {
+  output.innerHTML = "";
+});
+
+
+
+
+// //Calculator display
+// let display = [];
+
+
+// // // //Clear function
+// clear.addEventListener("click", () => {
+//   display = [""];
+//   output.innerHTML = 0;
+// });
+
+// //Display the value of each button on click
+
+// buttons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     if (button.className.match("btn2")) {
+//       display.push(button.value);
+//       output.innerHTML = display.join("");
+//       console.log(output.innerHTML);
+//     }
+
+//   });
+// });
+
+
+//Function that takes three parameters
+// function calculator(num1, operator, num2) {
+//   if (operator === "+") {
+//     return num1 + num2;
+//   } else if (operator === "-") {
+//     return num1 - num2;
+//   } else if (operator === "*") {
+//     return num1 * num2;
+//   } else if (operator === "/") {
+//     return num1 / num2;
+//   } else if (operator === "&radic") {
+//     return Math.pow(num1, 1 / 2);
+//   } else if (operator === "^") {
+//     return Math.pow(num1, num2);
+//   } else {
+//     return "Please provide the correct operator";
+//   }
+// }
+
+// console.log(calculator(1, "+", 2));
